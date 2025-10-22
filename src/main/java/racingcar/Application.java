c0fa97cf8119cdc2a74aa6fe96061a43245c5808
BumjinKim <21900104@handong.ac.kr>
2025-10-22 16:16:36 +0900

    feat(main): Application 클래스 구조 생성 및 시작점 구현 - appfactory.settingRacingCarController를 통한 의존성 주입 - run 메서드 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+import racingcar.factory.AppFactory;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppFactory appFactory = new AppFactory();
+
+        RacingCarController racingCarController = appFactory.settingRacingCarController();
+        racingCarController.runRacingCar();
     }
 }
