bbe36ff7c5ac22f6bda3e6421f2d68a9351f3273
BumjinKim <21900104@handong.ac.kr>
2025-10-22 21:11:19 +0900

    feat(util): Validator 클래스 구조 생성 및 기능 구현 - validateEmptyInput(): 빈 문자열에 대한 검증 - validateCarName(): 자동차 이름을 검증하는 전체 플로우 	- validateCarNameFormat(): 자동차 이름의 문자 형식을 검증 	- validateDelimiterFormat(): 자동차 이름을 구분자에서 분리할 	  때의 형식 검증 	- validateCarNameLength(): 자동차 이름의 길이 검증 	- validateCarNameIsDuplicate(): 자동차 이름이 중복됐는지 검증

---
+++ src/main/java/racingcar/util/Validator.java
@@ -0,0 +1,55 @@
+package racingcar.util;
+
+import racingcar.enums.ErrorMessageTemplate;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator {
+    private static final int Limited_Name_Length = 5;
+    private static final String REGEXP_EXCEPT_CHAR_NUM = "[^a-zA-Z0-9가-힣ㄱ-ㅎㅏ-ㅣ]";
+
+    public static void validateEmptyInput(String user_input){
+        if (user_input.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessageTemplate.INVALID_EMPTY_INPUT.getMessage());
+        }
+    }
+
+    public static void validateCarName(List<String> carNameList) {
+        carNameList.forEach(carName -> {
+            validateCarNameFormat(carName);
+            validateDelimiterFormat(carName);
+            validateCarNameLength(carName);
+        });
+
+        validateCarNameIsDuplicate(carNameList);
+    }
+
+    private static void validateCarNameFormat(String carName) {
+        Pattern pattern = Pattern.compile(REGEXP_EXCEPT_CHAR_NUM);
+        Matcher matcher = pattern.matcher(carName);
+
+        if (matcher.find()) {
+            throw new IllegalArgumentException(ErrorMessageTemplate.INVALID_CAR_NAME_FORMAT.getMessage());
+        }
+    }
+
+    private static void validateDelimiterFormat(String carName) {
+        if (carName.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessageTemplate.INVALID_DELIMITER_FORMAT.getMessage());
+        }
+    }
+
+    private static void validateCarNameLength(String carName) {
+        if (carName.length() > Limited_Name_Length) {
+            throw new IllegalArgumentException(ErrorMessageTemplate.INVALID_OVER_CAR_NAME_LENGTH.getMessage() + carName + " " + carName.length());
+        }
+    }
+
+    private static void validateCarNameIsDuplicate(List<String> carNameList) {
+        if (carNameList.size() != carNameList.stream().distinct().count()) {
+            throw new IllegalArgumentException(ErrorMessageTemplate.INVALID_DUPLICATE_CAR_NAME.getMessage());
+        }
+    }
+}
