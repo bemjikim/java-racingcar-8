d1c3961c541c17de897f1717de264aa645ed1911
BumjinKim <21900104@handong.ac.kr>
2025-10-22 21:09:56 +0900

    feat(enums): ErrorMessageTemplate 메시지 추가 - 자동차 이름 문자 형식 예외 처리 메시지 추가 - 구분자 형식 예외 처리 메시지 추가 - 자동차 이름 길이 예외 처리 메시지 추가 - 자동차 이름 중복 예외 처리 메시지 추가

--- src/main/java/racingcar/enums/ErrorMessageTemplate.java
+++ src/main/java/racingcar/enums/ErrorMessageTemplate.java
@@ -1,7 +1,11 @@
 package racingcar.enums;
 
 public enum ErrorMessageTemplate {
-    INVALID_EMPTY_INPUT("빈 문자열의 입력이 감지됐습니다! 1이상의 문자열을 입력해주세요");
+    INVALID_EMPTY_INPUT("빈 문자열의 입력이 감지됐습니다! 길이가 1 이상의 입력을 입력해주세요."),
+    INVALID_CAR_NAME_FORMAT("자동차 이름은 알파벳, 숫자, 한글을 제외한 문자를 허용하지 않습니다."),
+    INVALID_DELIMITER_FORMAT("',' 구분자의 형식이 잘못 됐습니다. ex) a, b, c"),
+    INVALID_OVER_CAR_NAME_LENGTH("각 자동차의 이름의 길이는 5이하로 제한됩니다! 입력한 이름과 길이: "),
+    INVALID_DUPLICATE_CAR_NAME("각 자동차의 이름은 중복될 수 없습니다: ");
 
     private final String message;
 
