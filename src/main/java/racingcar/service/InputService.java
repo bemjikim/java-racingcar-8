faabed281f5ed348dea25f94ba74b3a5fc4ec5a1
BumjinKim <21900104@handong.ac.kr>
2025-10-22 21:03:01 +0900

    feat(service): InputService 클래스 수정 - 코드 refactoring 및 공백 확인 함수 생성

--- src/main/java/racingcar/service/InputService.java
+++ src/main/java/racingcar/service/InputService.java
@@ -1,23 +1,35 @@
 package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.view.InputView;
 
 import java.util.NoSuchElementException;
 
+import static racingcar.util.Validator.validateEmptyInput;
+
 public class InputService {
     public String readNameInput() {
-        try {
-            return Console.readLine();
-        } catch (NoSuchElementException e) {
-            return "";
-        }
+        InputView.printNameInputUI();
+
+        return checkEmptyInput();
     }
 
     public String readTryInput() {
+        InputView.printTryInputUI();
+
+        return checkEmptyInput();
+    }
+
+    private String checkEmptyInput() {
+        String user_input;
+
         try {
-            return Console.readLine();
+            user_input = Console.readLine();
         } catch (NoSuchElementException e) {
-            return "";
+            user_input = "";
         }
+        validateEmptyInput(user_input);
+
+        return user_input;
     }
 }
