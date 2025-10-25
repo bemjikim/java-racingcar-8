399ef868ae20de2db29928562b2844b9b5a803dc
BumjinKim <21900104@handong.ac.kr>
2025-10-25 15:21:34 +0900

    feat(util): RandomNumberGenerator 클래스 구조 생성 및 기능 추가 - 0~9사이의 숫자를 랜덤으로 추출하는 기능 추가

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -1,5 +1,10 @@
 package racingcar.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandomNumberGenerator {
     // TODO:  camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange() 를 활용하여 랜덤 숫자 추출
+    public static int generateRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
+    }
 }
