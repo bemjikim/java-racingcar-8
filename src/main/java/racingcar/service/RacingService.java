9ef4886357cfc3dd5e336a2f17fcdb399f7d449b
BumjinKim <21900104@handong.ac.kr>
2025-10-25 15:24:40 +0900

    feat(service): RacingService 클래스 기능 추가 - CarList를 통해 자동차들을 등록하는 함수 추가 - 랜덤한 숫자를 뽑아 라운드를 진행하는 함수 추가 - 라운드를 진행할 때 진행 상황을 출력하는 함수 추가 - 자동차들 중 우승자를 찾는 함수 추가

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,5 +1,61 @@
 package racingcar.service;
 
+import racingcar.dto.CarList;
+import racingcar.enums.ErrorMessageTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static racingcar.util.RandomNumberGenerator.generateRandomNumber;
+
 public class RacingService {
-    // TODO: util패키지의 랜덤 숫자 Generator를 받아 경주 시작하는 기능 구현
+
+    private List<CarList> carInfoList;
+    private static final int MOVING_FORWARD = 4;
+    private static final String MOVING_MARK = "-";
+
+    public void setupCarNameList(List<String> carNameList){
+        carInfoList = new ArrayList<>();
+
+        for (String name:  carNameList){
+            carInfoList.add(registCarName(name, 0));
+        }
+    }
+
+    public CarList registCarName(String name, int moveCount) {
+        return new CarList(name, moveCount);
+    }
+
+    public void roundStart() {
+        for(CarList carList : carInfoList) {
+            int randomNumber = generateRandomNumber();
+
+            if(randomNumber >= MOVING_FORWARD) {
+                carList.moveFoward();
+            }
+
+            roundBroadcasting(carList);
+        }
+        System.out.println();
+    }
+
+    public void roundBroadcasting(CarList carList) {
+        System.out.println(carList.getName() + " : " + MOVING_MARK.repeat(carList.getMoveCount()));
+    }
+
+    public List<String> findWinner() {
+        int maxMoveCount = carInfoList.stream()
+                                      .mapToInt(CarList::getMoveCount)
+                                      .max()
+                                      .orElseThrow(() -> new IllegalArgumentException(ErrorMessageTemplate.NONE_FOUND_MAX_VALUE.getMessage()));
+
+        List<String> winnerList = carInfoList.stream()
+                                             .filter(CarList -> CarList.getMoveCount() == maxMoveCount)
+                                             .map(CarList::getName)
+                                             .toList();
+
+        return winnerList;
+    }
+
+
 }
