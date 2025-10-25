2ea17c7126f17c03b87eaae9b907735fd33e6043
BumjinKim <21900104@handong.ac.kr>
2025-10-25 15:22:26 +0900

    feat(enums): ErrorMessageTemplate 클래스 메시지 추가 - 시도횟수에 대한 예외처리 메시지 추가 - 최대값에 대한 예외처리 메시지 추가

--- src/main/java/racingcar/enums/ErrorMessageTemplate.java
+++ src/main/java/racingcar/enums/ErrorMessageTemplate.java
@@ -5,7 +5,11 @@ public enum ErrorMessageTemplate {
     INVALID_CAR_NAME_FORMAT("자동차 이름은 알파벳, 숫자, 한글을 제외한 문자를 허용하지 않습니다."),
     INVALID_DELIMITER_FORMAT("',' 구분자의 형식이 잘못 됐습니다. ex) a, b, c"),
     INVALID_OVER_CAR_NAME_LENGTH("각 자동차의 이름의 길이는 5이하로 제한됩니다! 입력한 이름과 길이: "),
-    INVALID_DUPLICATE_CAR_NAME("각 자동차의 이름은 중복될 수 없습니다: ");
+    INVALID_DUPLICATE_CAR_NAME("각 자동차의 이름은 중복될 수 없습니다: "),
+    INVALID_SMALLER_THAN_ZERO("시도 횟수의 입력으로 0이하의 값을 허용하지 않습니다."),
+    INVALID_NONE_NUMBER("시도 횟수의 입력으로 숫자가 아닌 다른 문자를 허용하지 않습니다."),
+    NONE_FOUND_MAX_VALUE("최대값을 찾지 못했습니다");
+
 
     private final String message;
 
