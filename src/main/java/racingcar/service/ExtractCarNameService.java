d77845cae76c6ad4d5412d3eeaeea9992ee1a8c7
BumjinKim <21900104@handong.ac.kr>
2025-10-25 15:10:41 +0900

    rename(service): DelimiterService -> ExtractCarNameService - 기능의 확실한 목적성을 보여주기 위해 이름을 다시 지음

--- src/main/java/racingcar/service/ExtractCarNameService.java
+++ src/main/java/racingcar/service/ExtractCarNameService.java
@@ -4,23 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static racingcar.util.Validation.validateCarName;
-
-// TODO: 이름 수정 필요! 밑에 메서드도
-public class DelimiterService {
+public class ExtractCarNameService {
     private static final String delimiter = ",";
 
     public List<String> extractCarName(String user_input) {
         List<String> carNameList = Arrays.asList(Stream.of(user_input.split(delimiter))
                                                        .map(String::trim)
                                                        .toArray(String[]::new));
-        validateCarNameList(carNameList);
 
         return carNameList;
-    }
-
-    // TODO: 이 validate 과정을 어디로 가져갈지 고민.. Controller로 옮기자!
-    public void validateCarNameList(List<String> carNameList) {
-        validateCarName(carNameList);
     }
 }
