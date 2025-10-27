dc06c8b2022aebec7bfc8cfd5e62f3322e1143d5
BumjinKim <21900104@handong.ac.kr>
2025-10-27 16:38:14 +0900

    refactor(controller): MainController, RacingController 리팩토링 - RandomNumberGenerator 의존성 주입 기능 생성

--- src/main/java/racingcar/controller/MainController.java
+++ src/main/java/racingcar/controller/MainController.java
@@ -3,6 +3,7 @@ package racingcar.controller;
 import racingcar.service.ExtractCarNameService;
 import racingcar.service.InputService;
 import racingcar.service.RacingService;
+import racingcar.util.RandomNumberGenerator;
 
 import java.util.List;
 
@@ -10,16 +11,18 @@ public class MainController {
     private final InputService inputService;
     private final ExtractCarNameService extractCarNameService;
     private final RacingService racingService;
+    private final RandomNumberGenerator randomNumberGenerator;
 
-    public MainController(InputService inputService, ExtractCarNameService extractCarNameService, RacingService racingService) {
+    public MainController(InputService inputService, ExtractCarNameService extractCarNameService, RacingService racingService, RandomNumberGenerator randomNumberGenerator) {
         this.inputService = inputService;
         this.extractCarNameService = extractCarNameService;
         this.racingService = racingService;
+        this.randomNumberGenerator = randomNumberGenerator;
     }
 
     public void runApp() {
         ExtractController extractController = new ExtractController(inputService, extractCarNameService);
-        RacingController racingController = new RacingController(inputService, racingService);
+        RacingController racingController = new RacingController(inputService, racingService, randomNumberGenerator);
 
         List<String> carNameList = extractController.runExtract();
         racingController.runRacingCar(carNameList);
--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -2,6 +2,7 @@ package racingcar.controller;
 
 import racingcar.service.InputService;
 import racingcar.service.RacingService;
+import racingcar.util.RandomNumberGenerator;
 import racingcar.view.OutputView;
 
 import java.util.List;
@@ -11,21 +12,23 @@ import static racingcar.util.Validator.*;
 public class RacingController {
     private final InputService inputService;
     private final RacingService racingService;
+    private final RandomNumberGenerator randomNumberGenerator;
 
-    public RacingController(InputService inputService, RacingService racingService) {
+    public RacingController(InputService inputService, RacingService racingService, RandomNumberGenerator randomNumberGenerator) {
         this.inputService = inputService;
         this.racingService = racingService;
+        this.randomNumberGenerator = randomNumberGenerator;
     }
 
     public void runRacingCar(List<String> carNameList) {
-        String tryInput = inputService.readTryInput();
+        String tryInput = inputService.readTryInput().trim();
         tryValidation(tryInput);
 
         racingService.setupCarNameList(carNameList);
 
         OutputView.printResultOutputUI();
         for(int i = 0; i < Integer.parseInt(tryInput); i++) {
-            racingService.roundStart();
+            racingService.roundStart(randomNumberGenerator);
         }
 
         List<String> winnerList = racingService.findWinner();
