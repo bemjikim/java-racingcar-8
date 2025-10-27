592675beadb75f2e27d53bcb600754f29cf5d3bc
BumjinKim <21900104@handong.ac.kr>
2025-10-27 16:40:06 +0900

    refactor(factory): AppFactory 리팩토링 - RandomNumberGenerator 클래스 의존성 주입

--- src/main/java/racingcar/factory/AppFactory.java
+++ src/main/java/racingcar/factory/AppFactory.java
@@ -4,21 +4,24 @@ import racingcar.controller.MainController;
 import racingcar.service.ExtractCarNameService;
 import racingcar.service.InputService;
 import racingcar.service.RacingService;
+import racingcar.util.RandomNumberGenerator;
 
 
 public class AppFactory {
     private final InputService inputService;
     private final ExtractCarNameService extractCarNameService;
     private final RacingService racingService;
+    private final RandomNumberGenerator randomNumberGenerator;
 
     public AppFactory() {
         this.inputService = new InputService();
         this.extractCarNameService = new ExtractCarNameService();
         this.racingService = new RacingService();
+        this.randomNumberGenerator = new RandomNumberGenerator();
     }
 
     public MainController settingMainController() {
-        return new MainController(inputService, extractCarNameService, racingService);
+        return new MainController(inputService, extractCarNameService, racingService, randomNumberGenerator);
     }
 }
 
