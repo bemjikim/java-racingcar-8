eaf80448e4d21f6de5583d143ce02458a573b7f1
BumjinKim <21900104@handong.ac.kr>
2025-10-27 16:36:38 +0900

    feat(util): NumberGenerator, RandomNumbergenerator 클래스 구조 생성 및 수정 - NumberGenrator Interface 생성 - RandomNumberGenerator가 NumberGenrator의 generator부분 Override

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -2,9 +2,9 @@ package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomNumberGenerator {
-    // TODO:  camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange() 를 활용하여 랜덤 숫자 추출
-    public static int generateRandomNumber() {
+public class RandomNumberGenerator implements NumberGenerator {
+    @Override
+    public int generate() {
         return Randoms.pickNumberInRange(0, 9);
     }
 }
